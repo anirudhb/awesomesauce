dfc2fd96b413656756e2129bb02b3f556124ff6e
anirudhb <anirudhqazxswced@gmail.com>
2016-04-08 17:35:09 -0700

    Added animation
    Added starter code, and an animation :open_mouth:

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
+// Electron
 var electron = require("electron");
+// The app
 var app = electron.app;
+// BrowserWindow
 var BrowserWindow = electron.BrowserWindow;
 
+// main window
 var mainWindow = null;
 
+// If every window is closed and the platform is NOT OS X, terminate the app
 app.on("window-all-closed", function() {
 	if (process.platform != "darwin") {
 		app.quit();
 	}
 });
 
+
+// When the app is ready, create a BrowserWindow and load a file
 app.on("ready", function() {
 	mainWindow = new BrowserWindow({
 		width: 800,
@@ -19,6 +26,9 @@ app.on("ready", function() {
 	
 	mainWindow.loadURL("file://" + __dirname + "/index.html");
 	
+	mainWindow.maximize();
+	
+	// When the window is closed, dereference the mainWindow variable
 	mainWindow.on("closed", function() {
 		mainWindow = null;
 	});
